bb75bd4a68479fce19092f11a5fc2ab3a4411a69
Hirtik Malvi <hirtik.malvi@gmail.com>
2026-03-13 13:55:27 +0530

    feat: add Result.cs

---
+++ backend/ProductManagementSystem.API/Common/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace ProductManagementSystem.API.Common
+{
+    public class Result<T>
+    {
+        public bool Success { get; set; }
+        public int StatusCode { get; set; }
+        public T? Data { get; set; }
+        public List<string>? Errors { get; set; }
+
+        public static Result<T> Ok(T data, int statusCode)
+        {
+            return new Result<T>
+            {
+                Success = true,
+                StatusCode = statusCode,
+                Data = data
+            };
+        }
+
+        public static Result<T> Fail(int statusCode, string[] errors)
+        {
+            return new Result<T>
+            {
+                Success = false,
+                StatusCode = statusCode,
+                Errors = errors.ToList()
+            };
+        }
+    }
+}
