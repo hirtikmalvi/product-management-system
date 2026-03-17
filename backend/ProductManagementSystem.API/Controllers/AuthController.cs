2b1db0c8012e5e66b3372621bf588427fb54ba06
Hirtik Malvi <hirtik.malvi@gmail.com>
2026-03-18 00:21:43 +0530

    fix: remove unnecessary imports from authcontroller.cs

--- backend/ProductManagementSystem.API/Controllers/AuthController.cs
+++ backend/ProductManagementSystem.API/Controllers/AuthController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using ProductManagementSystem.API.Common;
 using ProductManagementSystem.API.DTOs.Auth.Login;
 using ProductManagementSystem.API.DTOs.Auth.Register;
